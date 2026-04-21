6e8328584186f22d4d3b543901e0bdd8e02adfec
RICARDO77x <archivosinfo77@gmail.com>
2026-04-21 15:49:04 +0000

    vending

---
+++ vending.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+//variables for Tortas Chuma
+float price1=74.99; //
+int quantity1=15;// es para poner un numero entero nada de decimales
+char select1="t";
+
+//variables for tacos piporro
+float price2=109.90
+int quantity2=10;
+char select2='p';
+
+//print stock of vending machine
+printf("vending Machine Stock\n");
+printf(------------------------\n");
+printf("Item name:tortas Chuma\n");
+
+//VARIABLES pizzas italianas
+
+
+
+
+}
