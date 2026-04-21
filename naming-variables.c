419dff26c3f460956aa22b619f4ef244f10a8011
RICARDO77x <archivosinfo77@gmail.com>
2026-04-21 15:28:55 +0000

    variables

---
+++ naming-variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// uso forzoso int main(void)
+// inicio de la operacio #include<stdio.h>
+int main(void)
+{
+int number;//there should not be the exclamation mark
+int test_value;//there should not be a exclamation mark
+int example1;//there should not be a 1 in the start
+float int_variables;//should be the variables
+printf("Variables created successfully.\n");
+
+
+}
