5bbbecac7730d3aa4499183ea4cc22e946360c22
RICARDO77x <archivosinfo77@gmail.com>
2026-04-28 15:26:57 +0000

    temperature.c

--- Rectangle.c
+++ Rectangle.c
@@ -4,15 +4,16 @@
 int main(void)
 {
 
-float length,width
-char
-
-
-
-
-Area: X
-Perimeter: Y
-
+int length;
+int width;
+printf("Enter the length of the rectangle: \n");
+scanf("%d", &length);
+printf("Enter the width of the rectangle: \n");
+scanf("%d", &width);
+int Area =length*width;
+int Perimeter = (length+width)*2;
+printf("Area = %d\n",Area);
+printf("perimeter = %d\n",Perimeter);
 
 
 }
---
+++ temperature.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+int Fahrenheit;
+int Celsius;
+printf("temperatura deseada en Fahrenheit: \n");
+scanf("%F", &Fahrenheit);
+float °c = (temperature 32)/1.8;
+prinf("Fahrenheit is equal to %f\n", temperature
+);
+
+
+
+
+
+}
