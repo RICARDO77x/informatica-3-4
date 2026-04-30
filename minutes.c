fcce84f8cd3f2ce56b7f2e27331224bf51ba7a3d
RICARDO77x <archivosinfo77@gmail.com>
2026-04-30 21:09:41 +0000

    tarea

--- minutes.c
+++ minutes.c
@@ -2,10 +2,23 @@
 
 int main(void)
 {
+ int total_minutes;
+ printf("Movie running time: ");
+ scanf("%d",&total_minutes);
 
+int start_h;
+int start_m;
+printf("Start time: ");
+scanf("%d:%d", &start_h, &start_m);
 
+ int hours = total_minutes / 60
+ int min = total_minutes % 60;
 
+int end_h=start_h + hours;
+int ende_m =start_m+min;
 
+printf("the movie will las %d hr and %d min.\n", hours, min);
+printf("End time: %d:d% \n" , end_h, end_m);
 
 
 
--- temperature.c
+++ temperature.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-int Fahrenheit;
-int Celsius;
-printf("temperatura deseada en Fahrenheit: \n");
-scanf("%F", &Fahrenheit);
-float °c = (temperature 32)/1.8;
-prinf("Fahrenheit is equal to %f\n", temperature);
-make te
+float Fahrenheit;
+float Celcius;
+printf("Enter to temperature to Fahrenheit: \n");
+scanf("%f" , &Fahrenheit);
+Celcius = (Fahrenheit-32)/1.8;
+printf("Celcius = %f\n",Celsius);
 
 
 
 }
+°
