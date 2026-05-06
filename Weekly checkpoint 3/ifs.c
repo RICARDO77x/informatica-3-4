6e206ea2b66350227c2af0c1c6b27e27419a3b89
RICARDO77x <archivosinfo77@gmail.com>
2026-05-06 14:53:41 +0000

    ifs.c

---
+++ Weekly checkpoint 3/ifs.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(void)
+{
+int a = 5; //equal sing means assignment
+int b = 5;
+int c = 10;
+
+printf("%d == %d is %d \n",a b, a == b);//Equal sing means corparison
+printf("%d == %d is %d \n",a c, a != b);//!= means "not equal"
+// create a program to check if a number is negative
+int number;
+printf("Enter a number: ");
+scanf("%d",&number);
+
+if(number<0){
+printf("%d is a negative number.\n",number);
+
+
+}
+
+
+
+
+
+
+}
