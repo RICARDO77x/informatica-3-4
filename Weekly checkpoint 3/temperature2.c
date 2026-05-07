42396e89d38fc4bca10968e62ee36f6eaaf1a36b
RICARDO77x <archivosinfo77@gmail.com>
2026-05-07 20:19:23 +0000

    temperature2.c

---
+++ Weekly checkpoint 3/temperature2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+float Fahrenheit;
+float Celcius;
+printf("Enter to temperature to Fahrenheit: \n");
+scanf("%f" , &Fahrenheit);
+Celcius = (Fahrenheit-32)/1.8;
+printf("Celcius = %f\n",Celsius);
+
+
+
+}
+°
