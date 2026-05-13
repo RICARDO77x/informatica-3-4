cb555734ee34d40cb2d71ff16929932895299d18
RICARDO77x <archivosinfo77@gmail.com>
2026-05-13 21:29:00 +0000

    tareas

---
+++ weekly checkpointy 5/temp3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void c_to_f(void);
+void f_to_c(void);
+
+int main(void){
+ //3. Call
+ c_to_f();
+f_to_c();
+  }
+
+ //2. Define
+ void c_to_f(void){
+   float c;
+   printf("Enter the temperature in Celsius: ");
+   scanf("%f", &c);
+   float f = (c * 1.8) + 32;
+   printf("%.2f°C = %.2f°F \n", c, f);
+ }
+
+ void f_to_c(void){
+   float f;
+   printf("Enter the temperature in Celsius: ");
+   scanf("%f", &c);
+   float f = (c * 1.8) + 32;
+   printf("%.2f°C = %.2f°F \n", c, f);
+ }
+
+
+
+printf("Temperature Convert \n");
+printf("1. Celsius to Fahrenheit \n");
+printf("2. Fahrenheit to Celsius\n");
+printf("Enter your option: ");
